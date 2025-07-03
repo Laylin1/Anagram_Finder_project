55b044fa2287ca817cdafd9082dd27848c75f49d
Laylin1 <artemkolioglo@gmail.com>
2025-07-03 16:38:48 +0300

    Changed read method, add new supp method, add test data

---
+++ anagrams/src/main/java/com/example/AnagramFinder.java
@@ -0,0 +1,68 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.sun.tools.javac.Main;
+
+public class AnagramFinder {
+
+    //  Read/Write/Change file. (User's file)
+    public static File wordsAFile = new File("anagrams/src/data/example.txt");
+
+    // Ready file for testts. Only Read.
+    public static InputStream input = Main.class.getClassLoader().getResourceAsStream("example.txt");
+
+    public static void main(String[] args) throws IOException {
+        
+        //Change readerType field on "file" for upload other file 
+        String readerType = "resource";
+
+        BufferedReader reader = getRader(readerType);
+
+        List<String> words = readWords(reader);
+
+        for(String word : words){
+            System.out.println(word);
+        }
+
+    }
+
+    // Method for read file throught Buffer
+    public static List<String> readWords(BufferedReader reader) throws IOException{
+        List<String> wordList = new ArrayList<>();
+
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                String[] words = line.trim().split("\\s+");
+                Collections.addAll(wordList, words); 
+            }
+            reader.close();
+
+
+        return wordList;
+    }
+
+    // Method for swap between user's file or ready file
+    public static BufferedReader getRader(String source) throws IOException {
+        BufferedReader reader;
+
+        if(source.equalsIgnoreCase("file")) {
+            reader = new BufferedReader(new FileReader(wordsAFile));
+        } else if(source.equalsIgnoreCase("resource")) {
+            reader = new BufferedReader(new InputStreamReader(input));
+        } else {
+            throw new IllegalArgumentException(source);
+        }
+
+        return reader;
+    }
+}
